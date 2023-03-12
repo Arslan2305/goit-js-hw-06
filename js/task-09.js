8d74b322598bf1d780235232060e75f41266e966
Arslan Ziiatdinov <airtraffic2013@gmail.com>
2023-03-12 13:45:10 +0300

    в процесі

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,10 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+document.querySelector(".change-color").addEventListener("click", () => {
+  const color = getRandomHexColor();
+
+  document.querySelector("body").style.backgroundColor = color;
+  document.querySelector(".color").textContent = color;
+})
